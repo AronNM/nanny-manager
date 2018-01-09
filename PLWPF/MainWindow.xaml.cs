834f9841aba16ef5d96b561f1d09ec3264f30b1c
aharon michaels <aharonmichaels@gmail.com>
2018-01-09 11:29:43 +0200

    beginning of window creation

--- PLWPF/MainWindow.xaml.cs
+++ PLWPF/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using BL;
 using BE;
+using PLWPF.windows;
 
 namespace PLWPF
 {
@@ -23,9 +24,7 @@ namespace PLWPF
 	public partial class MainWindow : Window
 	{
 		IBL bl;
-		public Window1() { bl = FactoryBL.getBL(); }
-
-		int a;
+		
 
 		public MainWindow()
 		{
@@ -38,6 +37,17 @@ namespace PLWPF
 		private void choiceBoxUC_Loaded(object sender, RoutedEventArgs e)
 		{
 
+		}
+
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void Manage_Mothers_Click(object sender, RoutedEventArgs e)
+		{
+			ManageMothers manage_mothers = new ManageMothers();
+			manage_mothers.ShowDialog();
 		}
 	}
 }
---
+++ PLWPF/windows/ManageMothers.xaml.cs
@@ -0,0 +1,50 @@
+﻿using PLWPF.Add;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PLWPF.windows
+{
+	/// <summary>
+	/// Interaction logic for ManageMothers.xaml
+	/// </summary>
+	public partial class ManageMothers : Window
+	{
+		public ManageMothers()
+		{
+			InitializeComponent();
+		}
+
+		private void Add_Mother_Click(object sender, RoutedEventArgs e)
+		{
+			AddNewMother manage_mothers = new AddNewMother();
+			manage_mothers.ShowDialog();
+			manage_mothers.Show();
+		}
+
+		private void Edit_Mother_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void Delete_Mother_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void Show_All_Mothers_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+	}
+}
